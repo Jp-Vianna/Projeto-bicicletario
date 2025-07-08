2bfae840e21526f54825c96958af42578253f880
Jp-Vianna <jpnardyvianna@gmail.com>
2025-07-07 23:05:23 -0300

    Finalizando end-points
    Finalizando end-points necessários. Todos os casos de uso.

--- src/main/java/com/es2/bicicletario/service/AluguelService.java
+++ src/main/java/com/es2/bicicletario/service/AluguelService.java
@@ -15,6 +15,7 @@ import com.es2.bicicletario.dto.CiclistaRequestDTO;
 import com.es2.bicicletario.dto.CiclistaResponseDTO;
 import com.es2.bicicletario.dto.DevolucaoRequestDTO;
 import com.es2.bicicletario.dto.DevolucaoResponseDTO;
+import com.es2.bicicletario.dto.FuncionarioRequestDTO;
 import com.es2.bicicletario.dto.FuncionarioResponseDTO;
 import com.es2.bicicletario.entity.*;
 import com.es2.bicicletario.repository.*;
@@ -32,11 +33,6 @@ public class AluguelService {
     private final CiclistaRepository ciclistaRepository;
     private static final Logger logger = LoggerFactory.getLogger(AluguelService.class);
 
-    /**
-     * Cria um novo ciclista no sistema, aplicando validações de negócio.
-     * @param novoCiclista DTO com os dados para o novo ciclista.
-     * @return A entidade Ciclista persistida no banco de dados.
-     */
     @Transactional
     public CiclistaResponseDTO criarCiclista(CiclistaRequestDTO novoCiclista) {
 
@@ -84,145 +80,6 @@ public class AluguelService {
         return CiclistaResponseDTO.fromEntity(ciclistaSalvo);
     }
 
-
-    public Funcionario criarFuncionario(Funcionario novoFuncionario) {
-        return funcionarioRepository.save(novoFuncionario);
-    }
-
-    public Optional<CiclistaResponseDTO> getCiclistaById(Integer idCiclista) {
-    return ciclistaRepository.findById(idCiclista)
-        .map(CiclistaResponseDTO::fromEntity);
-    }
-
-    public Optional<FuncionarioResponseDTO> getFuncionarioById(String matricula) {
-    return funcionarioRepository.findById(matricula)
-        .map(FuncionarioResponseDTO::fromEntity);
-    }
-
-    public boolean permiteAluguel(Ciclista ciclista /*Entraria aqui a id da bicicleta */) {
-       
-        List<Status> statusDesejados = List.of(Status.EM_ANDAMENTO, Status.FINALIZADO_COM_COBRANCA_PENDENTE);
-        List<Aluguel> alugueis = aluguelRepository.findAllByCiclistaIdAndStatusIn(ciclista.getId(), statusDesejados);
-
-        return alugueis.isEmpty() && ciclista.getStatus().equals(Status.ATIVO);
-    }
-
-    public Integer getBicicletaAlugada(Integer idCiclista) { // Ainda falta integrar
-        Optional<Aluguel> optionalAluguel = aluguelRepository.findByCiclistaIdAndStatus(idCiclista, Status.EM_ANDAMENTO);
-        Aluguel aluguel = optionalAluguel.orElseThrow(() -> new RuntimeException("O ciclista não tem aluguéis no momento."));
-
-        if (aluguel == null) {
-            return null;
-        }
-
-        return aluguel.getIdBicicleta();
-    }
-
-    public boolean bicicletaEmReparo() { // A integrar
-        return false;
-    }
-
-
-    public Boolean existeEmail(String email) {
-        Ciclista ciclista = converteParaCiclista(ciclistaRepository.findByEmailEndereco(email));
-
-        return ciclista != null;
-    }
-
-    public List<FuncionarioResponseDTO> getFuncionarios() {
-        List<Funcionario> funcionarios = funcionarioRepository.findAll();
-        List<FuncionarioResponseDTO> funcDTO = new ArrayList<>();
-
-        for (Funcionario func : funcionarios) {
-            funcDTO.add(FuncionarioResponseDTO.fromEntity(func));
-        }
-        
-        return funcDTO;
-    }
-
-    public Boolean deletarFuncionario(String matricula) {
-        funcionarioRepository.deleteById(matricula);
-        return true;
-    }
-
-    public CartaoDeCredito getCartaoDeCredito(Integer idCiclista) {
-        Ciclista ciclista = converteParaCiclista(ciclistaRepository.findById(idCiclista));
-        
-        return ciclista.getCartao(); 
-    }
-
-    @Transactional
-    public AluguelResponseDTO realizarAluguel(AluguelRequestDTO novoAluguel) { // Integrar com trancas e bikes futuramente
-        
-        Ciclista ciclista = converteParaCiclista(ciclistaRepository.findById(novoAluguel.getIdCiclista()));
-
-        if (!permiteAluguel(ciclista)) {
-            throw new RegraDeNegocioException("O aluguel não foi autorizado.");
-        }       
-
-        Aluguel aluguel = new Aluguel();
-
-        aluguel.setTrancaInicial(novoAluguel.getIdTranca());
-        aluguel.setCiclista(ciclista);
-        aluguel.setHoraInicio(LocalDateTime.now());
-        aluguel.setIdBicicleta(10001);
-        aluguel.setStatus(Status.EM_ANDAMENTO);
-        
-        Aluguel aluguelSalvo = aluguelRepository.save(aluguel);
-
-        Cobranca.realizarCobrancaPadrao(); // A integrar, externo.
-
-        Tranca.destravaTranca();
-
-        return AluguelResponseDTO.fromEntity(aluguelSalvo);
-    }
-
-    public AluguelResponseDTO getAluguelByIdBicicleta(Integer idBicicleta) {
-        Optional<Aluguel> optionalAluguel = aluguelRepository.findByIdBicicletaAndStatus(idBicicleta, Status.EM_ANDAMENTO);
-        Aluguel aluguel = optionalAluguel.orElseThrow(() -> new RuntimeException("Nenhum aluguel ativo com esta bicicleta."));
-        
-        return AluguelResponseDTO.fromEntity(aluguel);
-    }
-
-    public Boolean trancaDisponivel() { // A integrar 
-        logger.warn("AVISO: Integração com API externa ainda não implementada. Usando comportamento FALSO de SUCESSO");
-
-        return true;
-    }
-
-    public DevolucaoResponseDTO realizarDevolucao(DevolucaoRequestDTO novaDevolucao) { // A integrar
-
-        Optional<Aluguel> optionalAluguel = aluguelRepository.findByIdBicicletaAndStatus(novaDevolucao.getIdBicicleta(), Status.EM_ANDAMENTO);
-        Aluguel aluguel = optionalAluguel.orElseThrow(() -> new RuntimeException("Não encontrou alugueis ativos com essa bicicleta."));
-
-        String valorTotal = Cobranca.realizarCobrancaExtra(); // A integrar
-
-        Devolucao devolucao = new Devolucao();
-        
-        devolucao.setTrancaFinal(novaDevolucao.getIdTranca());
-        devolucao.setHoraFim(LocalDateTime.now());
-        aluguel.setStatus(Status.FINALIZADO);
-        devolucao.setCobranca(new BigDecimal(valorTotal));
-
-        Aluguel aluguelAtualizado = aluguelRepository.save(aluguel); 
-
-        devolucao.setAluguel(aluguelAtualizado);
-        
-        Devolucao devolucaoCriado = devolucaoRepository.save(devolucao);
-
-        Tranca.travarTranca(); // Integrar
-
-        return DevolucaoResponseDTO.fromEntity(devolucaoCriado);
-    }
-
-    public Boolean validaTranca(/*Id da tranca */){
-        return true;//Integrar
-    }
-
-    public Boolean validaBicicleta(){ // Integrar
-        return true;
-    }
-
     @Transactional
     public CiclistaResponseDTO atualizarCiclista(Integer idCiclista, CiclistaRequestDTO ciclistaRequestDTO) {
         Ciclista ciclista = converteParaCiclista(ciclistaRepository.findById(idCiclista));
@@ -261,6 +118,35 @@ public class AluguelService {
         return CiclistaResponseDTO.fromEntity(ciclistaAtualizado);
     }
 
+    public Optional<CiclistaResponseDTO> getCiclistaById(Integer idCiclista) {
+        return ciclistaRepository.findById(idCiclista)
+            .map(CiclistaResponseDTO::fromEntity);
+    }
+
+    public CartaoDeCredito getCartaoDeCredito(Integer idCiclista) {
+        Ciclista ciclista = converteParaCiclista(ciclistaRepository.findById(idCiclista));
+        
+        return ciclista.getCartao(); 
+    }
+
+    @Transactional
+    public void atualizarCartaoDeCredito(Integer idCiclista, CiclistaRequestDTO.CartaoDeCreditoDto cartaoDeCreditoDto) {
+        Ciclista ciclista = converteParaCiclista(ciclistaRepository.findById(idCiclista));
+        
+        if (cartaoDeCreditoDto == null) {
+            throw new RegraDeNegocioException("Os dados do cartão de crédito não podem ser nulos.");
+        }
+
+        CartaoDeCredito novoCartao = cartaoDeCreditoDto.toEntity();
+        
+        if (!CartaoDeCredito.verificaCartao()) {
+            throw new RegraDeNegocioException("O cartão de crédito informado é inválido.");
+        }
+
+        ciclista.setCartao(novoCartao);
+        ciclistaRepository.save(ciclista);
+    }
+
     @Transactional
     public void ativarCiclista(Integer idCiclista) {
         Ciclista ciclista = converteParaCiclista(ciclistaRepository.findById(idCiclista));
@@ -271,6 +157,174 @@ public class AluguelService {
             throw new RegraDeNegocioException("O ciclista não está inativo.");
         }
     }
+
+    public Boolean existeEmail(String email) {
+        Ciclista ciclista = converteParaCiclista(ciclistaRepository.findByEmailEndereco(email));
+
+        return ciclista != null;
+    }
+
+
+    // Métodos do funcionário
+
+
+    public Funcionario criarFuncionario(Funcionario novoFuncionario) {
+        return funcionarioRepository.save(novoFuncionario);
+    }
+
+    @Transactional
+    public FuncionarioResponseDTO atualizarFuncionario(String matricula, FuncionarioRequestDTO funcionarioRequestDTO) {
+        Funcionario funcionario = funcionarioRepository.findById(matricula)
+                .orElseThrow(() -> new RegraDeNegocioException("Funcionário não encontrado com a matrícula: " + matricula));
+
+        // Atualiza os campos do funcionário com os dados do DTO
+        funcionario.setNome(funcionarioRequestDTO.getNome());
+        funcionario.setIdade(funcionarioRequestDTO.getIdade());
+        funcionario.setFuncao(funcionarioRequestDTO.getFuncao());
+        funcionario.setEmail(new Email(funcionarioRequestDTO.getEmail()));
+        
+        // Opcionalmente, atualiza a senha se for fornecida
+        if (funcionarioRequestDTO.getSenha() != null && !funcionarioRequestDTO.getSenha().isBlank()) {
+            if (!funcionarioRequestDTO.getSenha().equals(funcionarioRequestDTO.getConfirmacaoSenha())) {
+                throw new RegraDeNegocioException("A senha e a confirmação de senha não coincidem.");
+            }
+            funcionario.setSenha(funcionarioRequestDTO.getSenha());
+        }
+
+        Funcionario funcionarioAtualizado = funcionarioRepository.save(funcionario);
+        return FuncionarioResponseDTO.fromEntity(funcionarioAtualizado);
+    }
+
+    public Optional<FuncionarioResponseDTO> getFuncionarioById(String matricula) {
+    return funcionarioRepository.findById(matricula)
+        .map(FuncionarioResponseDTO::fromEntity);
+    }
+
+    public List<FuncionarioResponseDTO> getFuncionarios() {
+        List<Funcionario> funcionarios = funcionarioRepository.findAll();
+        List<FuncionarioResponseDTO> funcDTO = new ArrayList<>();
+
+        for (Funcionario func : funcionarios) {
+            funcDTO.add(FuncionarioResponseDTO.fromEntity(func));
+        }
+        
+        return funcDTO;
+    }
+
+    public Boolean deletarFuncionario(String matricula) {
+        funcionarioRepository.deleteById(matricula);
+        return true;
+    }
+
+
+    // Métodos de aluguel
+
+
+
+    public boolean permiteAluguel(Integer idCiclista) {
+       
+        Ciclista ciclista = this.converteParaCiclista(ciclistaRepository.findById(idCiclista));
+        
+        List<Status> statusDesejados = List.of(Status.EM_ANDAMENTO, Status.FINALIZADO_COM_COBRANCA_PENDENTE);
+        List<Aluguel> alugueis = aluguelRepository.findAllByCiclistaIdAndStatusIn(ciclista.getId(), statusDesejados);
+
+        return alugueis.isEmpty() && ciclista.getStatus().equals(Status.ATIVO);
+    }
+
+    public Integer getBicicletaAlugada(Integer idCiclista) { // Ainda falta integrar
+        Optional<Aluguel> optionalAluguel = aluguelRepository.findByCiclistaIdAndStatus(idCiclista, Status.EM_ANDAMENTO);
+        Aluguel aluguel = optionalAluguel.orElseThrow(() -> new RuntimeException("O ciclista não tem aluguéis no momento."));
+
+        if (aluguel == null) {
+            return null;
+        }
+
+        return aluguel.getIdBicicleta();
+    }
+
+    
+    @Transactional
+    public AluguelResponseDTO realizarAluguel(AluguelRequestDTO novoAluguel) { // Integrar com trancas e bikes futuramente
+        
+        Ciclista ciclista = converteParaCiclista(ciclistaRepository.findById(novoAluguel.getIdCiclista()));
+
+        if (!permiteAluguel(ciclista.getId())) {
+            throw new RegraDeNegocioException("O aluguel não foi autorizado.");
+        }       
+
+        Aluguel aluguel = new Aluguel();
+
+        aluguel.setTrancaInicial(novoAluguel.getIdTranca());
+        aluguel.setCiclista(ciclista);
+        aluguel.setHoraInicio(LocalDateTime.now());
+        aluguel.setIdBicicleta(10001);
+        aluguel.setStatus(Status.EM_ANDAMENTO);
+        
+        Aluguel aluguelSalvo = aluguelRepository.save(aluguel);
+
+        Cobranca.realizarCobrancaPadrao(); // A integrar, externo.
+
+        Tranca.destravaTranca();
+
+        return AluguelResponseDTO.fromEntity(aluguelSalvo);
+    }
+
+    public AluguelResponseDTO getAluguelByIdBicicleta(Integer idBicicleta) {
+        Optional<Aluguel> optionalAluguel = aluguelRepository.findByIdBicicletaAndStatus(idBicicleta, Status.EM_ANDAMENTO);
+        Aluguel aluguel = optionalAluguel.orElseThrow(() -> new RuntimeException("Nenhum aluguel ativo com esta bicicleta."));
+        
+        return AluguelResponseDTO.fromEntity(aluguel);
+    }
+
+    public DevolucaoResponseDTO realizarDevolucao(DevolucaoRequestDTO novaDevolucao) { // A integrar
+
+        Optional<Aluguel> optionalAluguel = aluguelRepository.findByIdBicicletaAndStatus(novaDevolucao.getIdBicicleta(), Status.EM_ANDAMENTO);
+        Aluguel aluguel = optionalAluguel.orElseThrow(() -> new RuntimeException("Não encontrou alugueis ativos com essa bicicleta."));
+
+        String valorTotal = Cobranca.realizarCobrancaExtra(); // A integrar
+
+        Devolucao devolucao = new Devolucao();
+        
+        devolucao.setTrancaFinal(novaDevolucao.getIdTranca());
+        devolucao.setHoraFim(LocalDateTime.now());
+        aluguel.setStatus(Status.FINALIZADO);
+        devolucao.setCobranca(new BigDecimal(valorTotal));
+
+        Aluguel aluguelAtualizado = aluguelRepository.save(aluguel); 
+
+        devolucao.setAluguel(aluguelAtualizado);
+        
+        Devolucao devolucaoCriado = devolucaoRepository.save(devolucao);
+
+        Tranca.travarTranca(); // Integrar
+
+        return DevolucaoResponseDTO.fromEntity(devolucaoCriado);
+    }
+
+
+    // Métodos de falsa integração
+
+
+    public Boolean trancaDisponivel() { // A integrar 
+        logger.warn("AVISO: Integração com API externa ainda não implementada. Usando comportamento FALSO de SUCESSO");
+
+        return true;
+    }
+
+    public Boolean validaTranca(/*Id da tranca */){
+        return true;//Integrar
+    }
+
+    public Boolean validaBicicleta(){ // Integrar
+        return true;
+    }
+
+    public boolean bicicletaEmReparo() { // A integrar
+        return false;
+    }
+
+
+    // Métodos de utilidade
 
 
     private Ciclista converteParaCiclista(Optional<Ciclista> optionalCiclista){
